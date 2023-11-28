7dfe7690ed4f1eea3bbc2b2b0ddc17efbf998112
starwars1211 <js34313431@naver.com>
2023-11-28 15:41:44 +0900

    11-28-2

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
+#include <iostream>
 #include "SDL.h"
 
 #pragma comment(lib, "SDL2")
 #pragma comment(lib, "SDL2main")
 
+
 int SDL_main(int argc, char* argv[])
+
 {
+	srand((unsigned int)(time(nullptr)));
+
 	SDL_Init(SDL_INIT_EVERYTHING);//// 초기화 모두 지우겟다
 
-	SDL_Window* MyWindow = SDL_CreateWindow("HelloWorld", 100, 100, 800, 600, SDL_WINDOW_OPENGL);  //////  앞에 2자리는 얼만큼 떨어지는지 뒤에 두자리는 해상도 
-	
+	SDL_Window* MyWindow = SDL_CreateWindow("HelloWorld", 100, 100, 800, 600, SDL_WINDOW_OPENGL);  ////// SDL창을 만들겠다   앞에 거리, 그다음 2자리는 크기 , 마지막은 해상도 
 
 	SDL_Renderer* MyRenderer = SDL_CreateRenderer(MyWindow, -1, SDL_RENDERER_ACCELERATED); //SDL_RENDERER_PRESENTVSYNC|SDL_RENDERER_TARGETTEXTURE
 	
@@ -17,12 +21,12 @@ int SDL_main(int argc, char* argv[])
 	while (IsRunning)/// message loop
 	{
 		SDL_PollEvent(&MyEvent);
-		switch (MyEvent.type)
+		switch (MyEvent.type) //// tick
 		{
-		case SDL_QUIT:
+		 case SDL_QUIT:
 		 IsRunning = false;
 		 break;
-		case SDL_KEYDOWN:
+		 case SDL_KEYDOWN:
 			if (MyEvent.key.keysym.sym == SDLK_ESCAPE)
 			{
 				IsRunning = false;
@@ -30,10 +34,26 @@ int SDL_main(int argc, char* argv[])
 			break;
 		}
 		// 명령어 죄다 저장 
-		SDL_SetRenderDrawColor(MyRenderer, 0xff, 0xff, 0xff, 0);
+		// 붓
+		SDL_SetRenderDrawColor(MyRenderer, 0xff, 0xff, 0xff, 0x00);
 		SDL_RenderClear(MyRenderer);
+		// Actors -> Render();
+		for (int i = 0; i < 100; ++i)
+		{
+			int X = rand() % 800;
+			int Y = rand() % 600;
+			int W = rand() % 200; + 10;
+			int H = rand() % 200; + 10;
+			int R = rand() % 256;
+			int G = rand() % 256;
+			int B = rand() % 256;
+			int A = rand() % 256;
 
-		SDL_RenderPresent(MyRenderer); /// 알파는 배경색
+			SDL_SetRenderDrawColor(MyRenderer, R, G, B, A);
+			SDL_Rect MyRect = {X,Y,W,H};
+			SDL_RenderFillRect(MyRenderer, &MyRect);
+		}
+		SDL_RenderPresent(MyRenderer);
 	}
 
 	SDL_Quit();///
